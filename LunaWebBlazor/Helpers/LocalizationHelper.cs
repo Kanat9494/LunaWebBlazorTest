6425ba307d74ea5810e696d88523a5a80d8f44d1
kana2022 <kkanat94@gmail.com>
2023-12-25 09:44:16 +0600

    Add project files.

---
+++ LunaWebBlazor/Helpers/LocalizationHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace LunaWebBlazor.Helpers;
+
+public class LocalizationHelper
+{
+    public LocalizationHelper(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    private readonly IConfiguration _config;
+    public RequestLocalizationOptions GetLocalizationOptions()
+    {
+        var cultures = _config.GetSection("Cultures").GetChildren()
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        var supportedCultures = cultures.Keys.ToArray();
+
+        var localizationOptions = new RequestLocalizationOptions()
+            .AddSupportedCultures(supportedCultures).AddSupportedCultures(supportedCultures);
+
+        return localizationOptions;
+    }
+}
